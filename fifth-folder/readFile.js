ace06b4d873c7f3e4354d3a2e5d16606923f6786
Jordi <jordi@iceverg.com>
2021-02-27 10:59:50 +0100

    summary events, exports, fs

--- fifth-folder/readFile.js
+++ fifth-folder/readFile.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const path = require('path');
+const {customEventEmitter} = require('../sixth-folder/events');
 
 fs.readFile(path.resolve('read.txt'), (err, data) => {
   if(err) {
     throw err;
   }
   console.log(`The content of the file is: ${data}`);
+  customEventEmitter.emit('done', 'Jordi');
 });
 
 console.log('Reading a file');
--- sixth-folder/events.js
+++ sixth-folder/events.js
@@ -5,8 +5,4 @@ eventEmitter.on('done', (name) => {
   console.log(`I am done ${name}`);
 })
 
-console.log(`Are you done?`);
-setTimeout(() => {
-  eventEmitter.emit('done', 'Peter');
-
-}, 1220)
+exports.customEventEmitter = eventEmitter;
