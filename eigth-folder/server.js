2aa4a736ad1f426d080e8dcd4049f26ea46b4b2b
Jordi <jordi@iceverg.com>
2021-03-01 12:26:05 +0100

    express generator

--- eigth-folder/server.js
+++ eigth-folder/server.js
@@ -2,11 +2,26 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  console.log(`query ${JSON.stringify(req.query, null, 2)}`);
+  console.log(`headers ${JSON.stringify(req.headers, null, 2)}`);
+  res.json({hey: 'ho'})
 })
 
 app.get('/hello', function (req, res) {
   res.send('You said hello')
+})
+
+app.post('/hello', function (req, res) {
+  console.log(`body payload ${JSON.stringify(req.body, null, 2)}`);
+  res.send('You post hello')
+})
+
+app.put('/hello', function (req, res) {
+  res.send('You put hello')
+})
+
+app.delete('/hello', function (req, res) {
+  res.send('You deleted hello')
 })
 
 app.listen(3000)
