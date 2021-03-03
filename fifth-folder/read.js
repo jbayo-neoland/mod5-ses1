0a14108ffa6c18f86efeeb686dd3bdd003f39a58
Jordi <jordi@iceverg.com>
2021-03-03 21:51:01 +0100

    solution to callbackHell

--- fifth-folder/read.js
+++ fifth-folder/read.js
@@ -3,31 +3,44 @@ const path = require('path');
 
 const fileName = path.resolve('read.txt');
 // get information about the file
-fs.exists(fileName, function(exists) {
+fs.exists(fileName, existsCallback);
+
+
+
+
+
+function existsCallback(exists) {
   if (exists) {
-
     // get information about the file
-    fs.stat(fileName, function(error, stats) {
-
-      // open the file (getting a file descriptor to it)
-      fs.open(fileName, "r", function(error, fd) {
-        let buffer = Buffer.alloc(stats.size);
-
-        // read its contents into buffer
-        fs.read(fd, buffer, 0, buffer.length/2, null, function(error, bytesRead, buffer) {
-
-          let data = buffer.toString("utf8", 0, buffer.length);
-
-          console.log(`And the content is \n${data}`);
-          fs.close(fd, (err, data) => {
-            if (err) {
-              throw err;
-            }
-          });
-        });
-      });
-    });
+    fs.stat(fileName, statCallback);
   }
-});
+}
 
+function statCallback(error, stats) {
+  // open the file (getting a file descriptor to it)
+  fs.open(fileName, "r", openCallbackWrapper(stats));
+}
+
+function openCallbackWrapper(stats){
+  return function openCallback(error, fd) {
+    let buffer = Buffer.alloc(stats.size);
+    // read its contents into buffer
+    fs.read(fd, buffer, 0, buffer.length/2, null, readCallbackWrapper(fd));
+  }
+}
+
+function readCallbackWrapper(fd) {
+  return function readCallback(error, bytesRead, buffer) {
+    let data = buffer.toString("utf8", 0, buffer.length);
+
+    console.log(`And the content is \n${data}`);
+    fs.close(fd, closeCallback);
+  }
+}
+
+const closeCallback = (err, data) => {
+  if (err) {
+    throw err;
+  }
+}
 console.log('Reading a file');
