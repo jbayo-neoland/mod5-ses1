fdb7e601e304f4c079443d1e7171a0b66a053719
Jordi <jordi@iceverg.com>
2021-03-03 21:39:08 +0100

    skeleton MVC

---
+++ ninth-folder/controllers/index.controller.js
@@ -0,0 +1,8 @@
+function index(req, res, next) {
+  // talk to model
+
+  // render view
+  res.send('hola!');
+}
+
+module.exports = index;
---
+++ ninth-folder/controllers/users.controller.js
@@ -0,0 +1,5 @@
+function user(req, res, next) {
+  res.send('respond with a resource');
+}
+
+module.exports = user;
