6845e5b0ad9eaa592e92b0d5a83780823b26bc21
Jordi <jordi@iceverg.com>
2021-03-03 17:19:53 +0100

    Solution to progressbar

--- third-folder/readline.js
+++ third-folder/readline.js
@@ -22,9 +22,23 @@ const main = async () => {
     console.log(`Input2 is ${chalk.blue(input2)}`)
     console.log('And the result is...');
     // todo: progressbar
-    setTimeout(() => {
-      console.log(`${parseInt(input1) * parseInt(input2)}`)
-    }, 1300);
+    const progress = require('progressbar').create().step('the task you are currently performing')
+    // use an array of steps that execute one second after each other
+    // as if we do them all instantly
+    // you won't see the progress bar as it will be instant
+    ;[
+        () => progress.setTotal(5),
+        () => progress.setTick(1),
+        () => progress.setTick(2),
+        () => progress.setTick(3),
+        () => progress.addTick(),
+        () => progress.addTick(),
+        () => progress.finish(() => {
+          console.log(`${parseInt(input1) * parseInt(input2)}`)
+        })  // remove and destroy the progress bar
+    ].forEach(function (step, index) {
+        setTimeout(step, index * 1000)
+    })
   } catch (e) {
     console.log(chalk.red(e));
   }
